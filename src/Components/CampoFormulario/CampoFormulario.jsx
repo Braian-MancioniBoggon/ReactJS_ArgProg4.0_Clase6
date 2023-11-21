05e09e447992163d258ab99dc0285a44e2cc50c9
Braian-MancioniBoggon <104107377+Braian-MancioniBoggon@users.noreply.github.com>
2023-11-21 16:46:01 -0300

    arreglado isInvalid

--- src/Components/CampoFormulario/CampoFormulario.jsx
+++ src/Components/CampoFormulario/CampoFormulario.jsx
@@ -6,7 +6,7 @@ import { InputNormal } from '../InputNormal/InputNormal'
 const CampoFormulario = ({datoSolicitado, nombreDato, tipoDato, error, value, touched, change }) => {
     let esPassword = {tipoDato}
     return(
-            <FormControl isRequired isInvalid={{error} && {touched}} mb={6}>
+            <FormControl isRequired isInvalid={error && touched} mb={6}>
                 <FormLabel>{datoSolicitado}</FormLabel>
                 {esPassword.tipoDato == "password" ? <InputPassword datoSolicitado={datoSolicitado} nombreDato={nombreDato} value={value} change={change}/> : <InputNormal datoSolicitado={datoSolicitado} nombreDato={nombreDato} tipoDato={tipoDato} value={value} change={change}/>}
             <FormErrorMessage mt={0} position='absolute'>
